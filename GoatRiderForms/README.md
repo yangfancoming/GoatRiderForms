78f6b513ad91feb24141accb53eb2f31af9c289b
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-01-16 09:17:05 +0800

    增加    chapter2-4-1   选择文件/文件夹 控件

--- GoatRiderForms/README.md
+++ GoatRiderForms/README.md
@@ -4,5 +4,5 @@
     chapter1-6-1   CheckListBox
     chapter1-7-1   ListBox
     
-    chapter2-4-1   ListBox
+    chapter2-4-1   选择文件/文件夹 控件
     
--- GoatRiderForms/chapter2-4-1/Form12411.cs
+++ GoatRiderForms/chapter2-4-1/Form12411.cs
@@ -7,14 +7,26 @@ namespace GoatRiderForms {
             InitializeComponent();
         }
 
+        // 选择文件对话框
         private void btn1_Click(object sender, EventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            // 指定 对话框打开后的初始化目录
             openFileDialog.InitialDirectory="c:\\";
             openFileDialog.Filter="文本文件|*.*|C#文件|*.cs|所有文件|*.*";
             openFileDialog.RestoreDirectory=true;
+            openFileDialog.Multiselect=false;//该值确定是否可以选择多个文件
             openFileDialog.FilterIndex=1;
             if (openFileDialog.ShowDialog()==DialogResult.OK){
                 textBox1.Text =  openFileDialog.FileName;
+            }
+        }
+
+        // 选择文件夹/目录对话框
+        private void btn2_Click(object sender, EventArgs e) {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK){
+                textBox1.Text = dialog.SelectedPath;
             }
         }
     }
